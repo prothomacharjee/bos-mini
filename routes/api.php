648fff8fadd6829ebb3972c6f5266c808f567aef
Prothom Acharjee <me.prothomacharjee@gmail.com>
2025-12-01 11:08:59 +0600

    reset sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth.sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/projects', [ProjectController::class, 'store']);
     Route::get('/projects/{project}', [ProjectController::class, 'show']);
     Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
