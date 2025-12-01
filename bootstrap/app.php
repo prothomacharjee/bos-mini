8213dc90568a78e74470e0c96859c935d5f77eb0
Prothom Acharjee <me.prothomacharjee@gmail.com>
2025-12-01 10:51:24 +0600

    remove extra/unwanted sanctum middleware check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'auth.sanctum' => EnsureFrontendRequestsAreStateful::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
